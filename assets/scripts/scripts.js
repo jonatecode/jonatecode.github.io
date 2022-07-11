ed18f4048ec233041cfe72e582230fc49fe0c480
Jonathan Ng <takeo.jonng@gmail.com>
2022-07-10 23:45:25 -0700

    test

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,44 +4,16 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    loop: true,
-    delay:50,
-    deleteSpeed:5
+  loop: true,
+  delay: 75,
 });
 
 typewriter
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString('nice to meet you.')
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("my name is christine.")
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("i'm a student at uc berkeley (go bears!).")
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("i'm studying computer science and data science.")
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("i'm also interested in design.")
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("fan of pineapple on pizza.")
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("i also like to doodle.")
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("thanks for visiting!")
-    .pauseFor(1000)
-    .deleteAll()
-    .start();
+  .pauseFor(2500)
+  .typeString('mommy')
+  .pauseFor(300)
+  .deleteChars(10)
+  .typeString('daddy')
+  .typeString('pp')
+  .pauseFor(1000)
+  .start();
