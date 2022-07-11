07c7facedd8280b0bacaad12ae570bb99260a85d
Jonathan Ng <takeo.jonng@gmail.com>
2022-07-11 16:00:29 -0700

    delSpeed

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -7,13 +7,14 @@ var app = document.getElementById('app');
 var typewriter = new Typewriter(app, {
   loop: true,
   delay: 50,
+  deleteSpeed: 5
 });
 
 typewriter
   .pauseFor(1000)
   .typeString('hi there!')
   .pauseFor(1500)
-  .deleteChars(10)
+  .deleteAll()
   .pauseFor(1000)
   .typeString('my name is Jonathan.')
   .pauseFor(1500)
